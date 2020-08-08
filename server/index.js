73a9a5bd787247556fd1208855adf5fd6cec9caa
tokio <tokio0813@gmail.com>
2020-08-09 06:44:47 +0900

    サーバー側 Auth0 対応

--- server/index.js
+++ server/index.js
@@ -1,9 +1,7 @@
 const express = require('express')
-const app = express()
 const mongoose = require('mongoose')
 const config = require('./config/dev')
 const bodyParser = require('body-parser')
-
 const usersRoutes = require('./routes/users')
 const profileRoutes = require('./routes/profile')
 const postRoutes = require('./routes/post')
@@ -11,8 +9,22 @@ const categoryRoutes = require('./routes/category')
 const productsRoutes = require('./routes/products')
 const searchPostRoutes = require('./routes/searchPost')
 
+const cors = require("cors");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const jwt = require("express-jwt");
+const jwksRsa = require("jwks-rsa");
+const authConfig = require("./auth_config.json");
+
+const appPort = process.env.SERVER_PORT || 3000;
+const appOrigin = authConfig.appOrigin || `http://localhost:${appPort}`;
+
+const app = express()
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(cors({ origin: appOrigin }));
 
 mongoose.connect(config.DB_URI, {
   useNewUrlParser: true,
@@ -33,8 +45,37 @@ app.use('/api/v1/category', categoryRoutes)
 app.use('/api/v1/products', productsRoutes)
 app.use('/api/v1/search', searchPostRoutes)
 
+// const port = process.env.API_PORT || 3001;
+// const appPort = process.env.SERVER_PORT || 3000;
+// const appOrigin = authConfig.appOrigin || `http://localhost:${appPort}`;
+
+if (!authConfig.domain || !authConfig.audience) {
+  throw new Error(
+    "Please make sure that auth_config.json is in place and populated"
+  );
+}
+
+const checkJwt = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://${authConfig.domain}/.well-known/jwks.json`
+  }),
+
+  audience: authConfig.audience,
+  issuer: `https://${authConfig.domain}/`,
+  algorithms: ["RS256"]
+});
+
 const PORT = process.env.PORT || '3001'
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// app.get('/', (req, res) => res.send('Hello World!'))
+app.get("/api/external", checkJwt, (req, res) => {
+  res.send({
+    msg: "Your access token was successfully validated!"
+  });
+});
 
-app.listen(PORT, () => console.log('I am running!'))
+app.listen(PORT, () => console.log(`I am running on port ${PORT}`));
+// app.listen(port, () => console.log(`API Server listening on port ${port}`));
--- server/routes/post.js
+++ server/routes/post.js
@@ -1,50 +1,52 @@
-const express = require('express')
-const router = express.Router()
-const Post = require('../model/post')
+const express = require('express');
+const router = express.Router();
+const Post = require('../model/post');
 
-router.get('', (req, res) => {
-  Post.find({}, function(err, foundPost) {
-    return res.json(foundPost)
-  })
-})
+router.get('/user/:uid', (req, res) => {
+  const uid = req.params.uid;
+  console.log(uid);
+  Post.find({ uid: uid }, function (err, foundPost) {
+    return res.json(foundPost);
+  });
+});
 
 router.get('/:postId', (req, res) => {
-  const postId = req.params.postId
-  Post.findById(postId, function(err, foundPost) {
-    if(err) {
-      return res.status(422).send(
-        { errors: [{ title: 'Error', detail: 'Post not found' }]}
-      )
+  const postId = req.params.postId;
+  Post.findById(postId, function (err, foundPost) {
+    if (err) {
+      return res
+        .status(422)
+        .send({ errors: [{ title: 'Error', detail: 'Post not found' }] });
     }
-    return res.json(foundPost)
-  })
-})
+    return res.json(foundPost);
+  });
+});
 
 router.post('', (req, res) => {
-  const ArticlePost = new Post()
+  const ArticlePost = new Post();
 
-  ArticlePost.title = req.body.title
-  ArticlePost.text = req.body.text
-  ArticlePost.tag = req.body.tag
-  ArticlePost.category = req.body.category
-  ArticlePost.url = req.body.url
-  ArticlePost.created = req.body.created
+  ArticlePost.uid = req.body.uid;
+  ArticlePost.title = req.body.title;
+  ArticlePost.text = req.body.text;
+  ArticlePost.tag = req.body.tag;
+  ArticlePost.category = req.body.category;
+  ArticlePost.url = req.body.url;
+  ArticlePost.created = req.body.created;
 
-  ArticlePost.save(function(err) {
-    if(err) {
-      res.send(err)
+  ArticlePost.save(function (err) {
+    if (err) {
+      res.send(err);
     } else {
-      res.json({ articlepost: 'success' })
+      res.json({ articlepost: 'success' });
     }
-  })
-})
+  });
+});
 
 router.delete('/:postId', (req, res) => {
-  const postId = req.params.postId
-  Post.deleteOne({_id: postId})
-    .then(function() {
-      res.json({ delete: 'success' })
-    })
-})
+  const postId = req.params.postId;
+  Post.deleteOne({ _id: postId }).then(function () {
+    res.json({ delete: 'success' });
+  });
+});
 
-module.exports = router
+module.exports = router;
